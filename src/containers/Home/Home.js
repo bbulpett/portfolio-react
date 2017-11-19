d5c6b743ec3acf1942d1fc537fa76460f841ee08
Barnabas Bulpett <barnabasbulpett@gmail.com>
2017-11-18 21:46:37 -0500

    Organize container files

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -10,6 +10,7 @@ export default class Home extends Component {
   render() {
     return (
       <Container className="App">
+        {/* TODO: Add an actual photo, perhaps a cinemagraph GIF */}
         <Image src={require('../assets/miitomo.jpg')} alt="miitomo-image"/>
         <p>
         Frosty Christmas Christmas elves jingle bells celebrate bells ornament holly. Wreath gift holiday chimney give reindeer nutcracker elves, chestnuts ornament candy cane calendar reindeer singing. Love ivy noel elves cookie, chestnuts chestnuts gold tree holiday ivy toys. Candy cane noel goodwill chimney goodwill ivy, candy cane stocking carols love ornament toys goodwill icicle. Noel gift give santa, give singing guest partridge calendar partridge bells.
