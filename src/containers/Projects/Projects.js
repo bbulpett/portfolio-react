15ef0b5579b27c6ca799f0f21adfd69a2680e842
Barnabas Bulpett <barnabasbulpett@gmail.com>
2017-12-02 07:31:00 -0500

    Add photos and display with react-zoomy

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -7,8 +7,10 @@ class Projects extends Component {
     return (
       <div>
         {<Zoomy
-          imageUrl={'https://images.unsplash.com/photo-1487309078313-fad80c3ec1e5?dpr=2&auto=format&fit=crop&w=767&h=1023&q=80&cs=tinysrgb&crop='}
-          renderThumbnail={({ showImage }) => <button onClick={showImage}>Show Image</button>}
+          imageUrl={require('../../assets/office-photo.jpg')}
+          renderThumbnail={({ showImage }) => 
+            <img src={require('../../assets/office-photo-small.jpg')} onClick={showImage} />
+        }
           scale={[1.1, 1.1]}
           imageProps={{
             style: {
