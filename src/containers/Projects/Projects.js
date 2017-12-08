b2c3c810961050ffc8c4a25db77ad4622e8d641e
Barnabas Bulpett <barnabasbulpett@gmail.com>
2017-12-08 07:54:43 -0500

    Styled components for projects page

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import Zoomy from 'react-zoomy';
+import {
+  ImageButton
+} from './Projects.style';
+import { Containter } from '../../theme/grid';
 
 class Projects extends Component {
   
@@ -9,7 +13,9 @@ class Projects extends Component {
         {<Zoomy
           imageUrl={require('../../assets/office-photo.jpg')}
           renderThumbnail={({ showImage }) => 
-            <img src={require('../../assets/office-photo-small.jpg')} onClick={showImage} />
+            <ImageButton onClick={showImage}>
+              <img src={require('../../assets/office-photo-small.jpg')} alt="office"/>
+            </ImageButton>
         }
           scale={[1.1, 1.1]}
           imageProps={{
