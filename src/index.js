55bcdb98757573c9429598241f6986b70fc6e509
Barnabas Bulpett <barnabasbulpett@gmail.com>
2018-01-31 22:01:54 -0500

    Add static generators

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { render } from 'react-static-generator';
 import routes from './routes';
 
 import './theme/globalStyle';
 
-ReactDOM.render(
+render(
   routes, 
   document.getElementById('root')
 );
