16a3fc90086b3bcf52409c6f43dc4a6922533185
Barnabas Bulpett <barnabasbulpett@gmail.com>
2018-02-10 13:55:44 -0500

    Import material-iconic stylesheet directly from component

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,6 +4,7 @@ import NavigationBar from '../../components/NavigationBar/NavigationBar';
 import { CurrentLocation } from '../../components/PageTitle/PageTitle';
 import ReallySmoothScroll from 'really-smooth-scroll';
 import { hashHistory } from 'react-router';
+import '../../assets/material-design-iconic-font/css/material-design-iconic-font.min.css';
 
 hashHistory.listen(() => {
   window.scrollTo(0, 0);
